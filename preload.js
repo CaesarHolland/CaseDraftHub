b33ef2e7bd7e66669b2ec468cbb2e181cf688605
houpuling <houpuling@msunsoft.com>
2022-02-18 17:12:38 +0800

    解决打包流程

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const defaultDataRootPath = "./data/";
 const defaultDataPath = ["./data/unfinished", "./data/finished"];
+const defaultConfPath = "./conf/"
 
 console.log("preload.js loaded");
 
@@ -16,6 +17,10 @@ try {
     }
     if (!fs.existsSync(defaultDataPath[1])) {
         fs.mkdirSync(defaultDataPath[1]);
+    }
+    if (!fs.existsSync(defaultConfPath)) {
+        fs.mkdirSync(defaultConfPath);
+        fs.writeFileSync(defaultConfPath + "model.txt", "年龄:\n现病史:\n个人史:\n住址:\n家族史:\n电话:\n体格检查:\n辅助检查:");
     }
 } catch (err) {
     console.log(err);
