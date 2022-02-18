1af81d794e358e5b18004c02ec1723a24199f07e
houpuling <houpuling@msunsoft.com>
2022-02-18 11:14:48 +0800

    修改preload中statSync为existSync，修复bug

--- preload.js
+++ preload.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 const defaultDataRootPath = "./data/";
 const defaultDataPath = ["./data/unfinished", "./data/finished"];
 
+console.log("preload.js loaded");
+
 try {
-    if (!fs.statSync(defaultDataRootPath)) {
+    if (!fs.existsSync(defaultDataRootPath)) {
         fs.mkdirSync(defaultDataRootPath);
         fs.mkdirSync(defaultDataPath[0]);
         fs.mkdirSync(defaultDataPath[1]);
     }
-    if (!fs.statSync(defaultDataPath[0])) {
+    if (!fs.existsSync(defaultDataPath[0])) {
         fs.mkdirSync(defaultDataPath[0]);
     }
-    if (!fs.statSync(defaultDataPath[1])) {
+    if (!fs.existsSync(defaultDataPath[1])) {
         fs.mkdirSync(defaultDataPath[1]);
     }
-} catch(err) {
+} catch (err) {
     console.log(err);
 }
